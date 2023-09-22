b7a1b2dfc1c5bffba723c13084bc89ef8da6a1d7
Mykola Postoiko <dotnicht@users.noreply.github.com>
2023-09-22 23:30:57 +0300

    send token

--- Quiiiz.Peon/Works/ExtractStuff.cs
+++ Quiiiz.Peon/Works/ExtractStuff.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Nethereum.Contracts.Standards.ERC20.ContractDefinition;
+using Nethereum.RPC.Eth.DTOs;
 using Quiiiz.Peon.Configuration;
 using Quiiiz.Peon.Domain;
 using Quiiiz.Peon.Persistence;
@@ -25,14 +27,35 @@ internal class ExtractStuff : IWork
     {
         foreach (var user in repository.Content)
         {
+            var web3 = blockchain.Value.CreateUser(user);
+
             if (options.Value.Token.Extract && user.Token > 0)
             {
-                throw new NotImplementedException();
+                var receipt = await web3.Eth.ERC20
+                    .GetContractService(blockchain.Value.TokenAddress)
+                    .TransferRequestAndWaitForReceiptAsync(new TransferFunction
+                    {
+                        AmountToSend = user.Token,
+                        To = options.Value.Token.Address
+                    }, cancellationToken);
+
+                if (receipt.Succeeded())
+                {
+                    logger.LogInformation("Extracted token from user {User}. Transaction {Hash}.", 
+                        await user.UpdateToken(repository, blockchain.Value),
+                        receipt.TransactionHash);
+                }
+                else
+                {
+                    logger.LogError("Transfer transaction failed {Hash}.", receipt.TransactionHash);
+                }
             }
 
             if (options.Value.Gas.Extract && user.Gas > 0)
             {
-                throw new NotImplementedException();
+                // TODO: transfer.
+
+                logger.LogInformation("Extracted gas from user {User}.", await user.UpdateGas(repository, blockchain.Value));
             }
         }
     }
